e6409e4e0746907293136fc6b81ebc9a1642a428
euglYA <90235668+euglYA@users.noreply.github.com>
2024-03-01 13:07:38 +0700

    second lab

---
+++ second/second.js
@@ -0,0 +1,52 @@
+const getRandomChoice = (probabilities) => {
+    const randomValue = Math.random();
+    let cumulativeProbability = 0;
+    for (const choice in probabilities) {
+        cumulativeProbability += probabilities[choice];
+        if (randomValue <= cumulativeProbability) {
+            return choice;
+        }
+    }
+}
+
+const players = {
+    first: {
+        rock: 1/3,
+        scissors: 1/6,
+        paper: 1/6,
+        well: 1/3
+    },
+    second: {
+        rock: 1/4,
+        scissors: 1/4,
+        paper: 1/4,
+        well: 1/4
+    },
+};
+
+const game = {
+    matrix: [
+        [0, 1, -1, -1], 
+        [-1, 0, 1, -1], 
+        [1, -1, 0,  1], 
+        [1,  1, -1, 0]
+    ],
+    index: {
+        rock: 0,
+        scissors: 1,
+        paper: 2,
+        well: 3
+    }
+}
+
+const count = 1000;
+
+let firstPlayerSum = 0;
+for (let i = 0; i < count; i++) {
+    const choices = {
+        first: game.index[getRandomChoice(players.first)],
+        second: game.index[getRandomChoice(players.second)]
+    }
+    firstPlayerSum += game.matrix[choices.first][choices.second] 
+}
+console.log(`First player average win, ${count} games: `, firstPlayerSum / count)
